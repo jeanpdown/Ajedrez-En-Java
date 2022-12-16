22bda73093dd1fb70fb38694ab4cd076dca9b705
YeyderJHJL <120533869+YeyderJHJL@users.noreply.github.com>
2022-12-15 19:25:51 -0500

    Update Reina.java
    se cambió movimiento diagonal y recto

--- src/Ventanas/Reina.java
+++ src/Ventanas/Reina.java
@@ -33,16 +33,14 @@ public class Reina extends Ficha{
     public void movimientoDiagonal(ArrayList<String> posicionesPosibles,Ficha fichas[][],int variacionY,int variacionX){
         
         for(int i=1;i<8;i++){
-            movimientoFichaSegunVariacion(posicionesPosibles,fichas,i*variacionY,i*variacionX);
-            if(comprobarRangoIndice(getPosY()+i*variacionY,getPosX()+i*variacionX) && fichas[getPosY()+i*variacionY][getPosX()+i*variacionX]!=null){
+            if(!movimientoFichaSegunVariacion(posicionesPosibles,fichas,i*variacionY,i*variacionX)){
                 break;
             }
         }
     }
     public void movimientoRecto(ArrayList<String> posicionesPosibles,Ficha fichas[][],int variacionY,int variacionX){
         for(int i=1;i<8;i++){
-            movimientoFichaSegunVariacion(posicionesPosibles,fichas,i*variacionY,i*variacionX);
-            if(comprobarRangoIndice(getPosY()+i*variacionY,getPosX()+i*variacionX) && fichas[getPosY()+i*variacionY][getPosX()+i*variacionX]!=null){
+            if(!movimientoFichaSegunVariacion(posicionesPosibles,fichas,i*variacionY,i*variacionX)){
                 break;
             }
         }
