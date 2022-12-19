50838b770fe16b502e952c8312dabe89211599bf
Jeanpiero HC <110795716+jeanpdown@users.noreply.github.com>
2022-12-19 00:37:23 -0500

    Update FondoPanel.java
    actualize para agregar imagen por url

--- src/Ventanas/FondoPanel.java
+++ src/Ventanas/FondoPanel.java
@@ -10,10 +10,15 @@ import javax.swing.JPanel;
 
 public class FondoPanel extends JPanel{
     private Image imagen;
+    private String url;
+
+    public FondoPanel(String url) {
+        this.url = url;
+    }
     
     @Override
     public void paint(Graphics g){
-        imagen=new ImageIcon(getClass().getResource("/imagenes/mesa1.jpg")).getImage();
+        imagen=new ImageIcon(getClass().getResource(url)).getImage();
         
         g.drawImage(imagen,0,0,getWidth(),getHeight(),this);
         setOpaque(false);
